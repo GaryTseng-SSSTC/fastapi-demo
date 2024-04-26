850f807da6e9fcb71f5758c6c5f58d377925a141
gary.tseng@ssstc.com <133744405+GaryTseng-SSSTC@users.noreply.github.com>
2024-04-26 08:26:58 +0000

    20240426-02

--- book.py
+++ book.py
@@ -2,26 +2,28 @@ from fastapi import FastAPI
 from fastapi import HTTPException
 import uvicorn
 
+from db import load_book
+
 app = FastAPI()
 
-books =
+books = load_book()
 
 
 @app.get("/api/books")
-def get_books(category: str|None = None, _id: int|None = None) -> list:
+def get_books(category: str|None = None, id_: int|None = None) -> list:
   result = books
   if category:
     result = [book for book in books if book['category'] == category]
-  if _id:
-    return [book for book in result if book['_id'] == _id]
+  if id_:
+    return [book for book in result if book['id_'] == id_]
   return result
 
-@app.get("/api/books/{_id}")
-def get_book_by_id(_id: int) -> dict:
-  result = [book for book in books if book['_id'] == _id]
+@app.get("/api/books/{id_}")
+def get_book_byid_(id_: int) -> dict:
+  result = [book for book in books if book['id_'] == id_]
   if result:
     return result[0]
-  raise HTTPException(status_code=404, detail=f"No book with id={_id}")
+  raise HTTPException(status_code=404, detail=f"No book with id={id_}")
 
 if __name__ == "__main__":
   uvicorn.run("book:app",reload=True)
